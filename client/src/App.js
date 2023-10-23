96c636b024f8a97b89ff834cd0c860e4f907e458
Jiří Vlček <ext.Jiri.Vlcek3@skoda-auto.cz>
2023-10-23 12:53:54 +0200

    05 - Update background

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ListOfRecipes from "./components/ListOfRecipes";
 
 function App() {
+
   return (
     <div className="App">
       <h1>CookBook</h1>
         <p>Aplikace vytvořená v ReactJS</p>
+      <div className="body">
         <ListOfRecipes/>
+      </div>
     </div>
   );
 }
